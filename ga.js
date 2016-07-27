be5e3519eafc28a64ed430f1a959d60de2352198
JoshCode-zz <mrjconroy@gmail.com>
2016-07-27 13:12:44 -0300

    litle

--- ga.js
+++ ga.js
@@ -1,7 +1,7 @@
 $(document).ready (function(){
   $("#subscribe-step-1").on("click", function (){
     ga('send', 'event', 'users', 'subscribe', 'landing-page', '1');
-  })
+  });
 
   $("#subscribe-step-2").on("click", function (){
     ga('send', 'event', 'users', 'subscribe', 'landing-page', '2');
